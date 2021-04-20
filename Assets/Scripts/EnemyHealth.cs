950b45a596356cdd9d4ab95c23070352a765d4d7
Mortumar <73446224+Mortumar@users.noreply.github.com>
2021-04-20 22:51:42 +0200

    20.04 health and Damage. Weapon VFX

---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField] float hitPoint = 100f;
+    public void TakeDamage(float damage)
+    {
+        hitPoint -= damage;
+        if (hitPoint <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
